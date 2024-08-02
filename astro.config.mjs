206c955559eb39cabf0d734f467696462e882884
Tan Thanh Nguyen <ngthanhtan91@gmail.com>
2024-08-02 14:28:16 +0800

    Add in UI Framework component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://myastrodemo.netlify.app"
+  site: "https://myastrodemo.netlify.app",
+  integrations: [preact()]
 });
